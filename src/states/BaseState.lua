896923c849501fe8d9b6cd6b241cc8947029d2f0
Nishith Gangajaliya <84803886+YoNishu@users.noreply.github.com>
2024-04-07 17:31:43 +0530

    Delete src directory

--- src/states/BaseState.lua
+++
@@ -1,7 +0,0 @@
-BaseState = Class{}
-
-function BaseState:init() end
-function BaseState:enter(param) end
-function BaseState:update(dt) end
-function BaseState:render() end
-function BaseState:exit() end
